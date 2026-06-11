1d8ef4bd660f3d612439d5eef05164e7b56ae84e
Valentino Grazioso <vgrazioso1@gmail.com>
2026-06-11 04:16:57 -0300

    toda la seccion de diseñador y solucion en titulos

---
+++ assets/js/products-disenador.js
@@ -0,0 +1,204 @@
+const catalogoDisenador = {
+  xerjoff: {
+    nombre: "Xerjoff",
+    productos: [
+      { nombre: "ERBA PURA EDP 100 ML", precio: "$552,600" },
+      { nombre: "ERBA GOLD EDP 100 ML", precio: "$633,700" },
+      { nombre: "NAXOS", precio: "$633,700" },
+      { nombre: "TORINO 21", precio: "$633,700" },
+    ]
+  },
+  montale: {
+    nombre: "Montale Paris",
+    productos: [
+      { nombre: "ARABIANS MUSK", precio: "$283,900" },
+      { nombre: "ARABIANS TONKA", precio: "$329,500" },
+    ]
+  },
+  tomford: {
+    nombre: "Tom Ford",
+    productos: [
+      { nombre: "ROSE PRICK 50 ML", precio: "$783,300" },
+      { nombre: "OMBRE LEATHER 100 ML", precio: "$633,700" },
+      { nombre: "OUD WOOD 100 ML", precio: "$1,140,700" },
+      { nombre: "NOIR 100 ML", precio: "$537,400" },
+      { nombre: "COSTA AZZURRA 100 ML", precio: "$608,400" },
+      { nombre: "LOST CHERRY 100 ML", precio: "$1,052,000" },
+    ]
+  },
+  parfumsdemarly: {
+    nombre: "Parfums De Marly",
+    productos: [
+      { nombre: "ORIANA 75 ML", precio: "$783,300" },
+      { nombre: "DELINA EXCLUSIF 75 ML", precio: "$783,300" },
+      { nombre: "LAYTON EXCLUSIF PARFUM 125 ML", precio: "$760,500" },
+    ]
+  },
+  mancera: {
+    nombre: "Mancera",
+    productos: [
+      { nombre: "INSTANT CRUSH 120 ML", precio: "$319,400" },
+      { nombre: "ROSE Y VANILLA 120 ML", precio: "$329,500" },
+      { nombre: "CEDRAT BOISE 120 ML", precio: "$354,900" },
+    ]
+  },
+  creed: {
+    nombre: "Creed",
+    productos: [
+      { nombre: "AVENTUS 100 ML", precio: "$846,600" },
+    ]
+  },
+  kurkdjian: {
+    nombre: "M.F. Kurkdjian",
+    productos: [
+      { nombre: "BACCARAT ROUGE 540", precio: "$973,400" },
+    ]
+  },
+  guess: {
+    nombre: "Guess",
+    productos: [
+      { nombre: "GUESS FOREVER EDP 75 ML", precio: "$136,800" },
+      { nombre: "GUESS BY MARCIANO EDP 100 ML", precio: "$136,800" },
+    ]
+  },
+  pacorabanne: {
+    nombre: "Paco Rabanne",
+    productos: [
+      { nombre: "ONE MILLION EDP FEM 80 ML", precio: "$233,200" },
+      { nombre: "ONE MILLION MEN EDT 100 ML", precio: "$195,100" },
+      { nombre: "PURE XS MEN 100 ML", precio: "$240,800" },
+      { nombre: "BLACK XS EDT 100 ML", precio: "$182,500" },
+      { nombre: "INVICTUS EDT 100 ML", precio: "$200,200" },
+      { nombre: "INVICTUS VICTORY EDP 100 ML", precio: "$235,700" },
+      { nombre: "INVICTUS VICTORY ELIXIR EDP 100 ML", precio: "$271,200" },
+    ]
+  },
+  carolinaherrera: {
+    nombre: "Carolina Herrera",
+    productos: [
+      { nombre: "LA BOMBA 100 ML", precio: "$370,100" },
+      { nombre: "GOOD GIRL 80 ML", precio: "$258,500" },
+      { nombre: "GOOD GIRL BLUSH 80 ML", precio: "$299,100" },
+      { nombre: "BAD BOY EDP 100 ML", precio: "$233,200" },
+      { nombre: "BAD BOY EDT 100 ML", precio: "$223,000" },
+      { nombre: "BAD BOY EDP 150 ML", precio: "$273,700" },
+      { nombre: "212 VIP BLACK EDP 100 ML", precio: "$228,100" },
+      { nombre: "212 VIP ROSE 80 ML", precio: "$228,100" },
+    ]
+  },
+  jpg: {
+    nombre: "Jean Paul Gaultier",
+    productos: [
+      { nombre: "SCANDAL POUR HOMME EDP 100 ML", precio: "$278,800" },
+      { nombre: "SCANDAL ABSOLU FOR HIM PARFUM 100 ML", precio: "$276,300" },
+      { nombre: "SCANDAL LE PARFUM REFILLABLE 100 ML", precio: "$273,700" },
+      { nombre: "LE BEAU LE PARFUM EDP", precio: "$273,700" },
+      { nombre: "LE BEAU LE PARFUM INTENSE EDP 125 ML", precio: "$329,500" },
+      { nombre: "LE BEAU PARADISE GARDEN 125 ML", precio: "$253,500" },
+      { nombre: "LE MALE ELIXIR ABSOLU", precio: "$319,400" },
+      { nombre: "LE MALE ELIXIR PARFUM 125 ML", precio: "$309,200" },
+      { nombre: "LE MALE LE PARFUM 125 ML", precio: "$278,800" },
+      { nombre: "LE MALE EAU DE TOILETTE 125 ML", precio: "$223,000" },
+      { nombre: "GAULTIER DIVINE LE PARFUM", precio: "$327,000" },
+      { nombre: "LA BELLE EDP 100 ML", precio: "$243,300" },
+      { nombre: "LA BELLE LE PARFUM INTENSE EDP 100 ML", precio: "$245,800" },
+    ]
+  },
+  armani: {
+    nombre: "Armani",
+    productos: [
+      { nombre: "AQUA DI GIOIA EDP 100 ML", precio: "$228,100" },
+      { nombre: "ACQUA DI GIO EDP 125 ML", precio: "$301,600" },
+      { nombre: "ACQUA DI GIO PARFUM 100 ML", precio: "$327,000" },
+      { nombre: "ACQUA DI GIO PROFONDO EDP 100 ML", precio: "$301,600" },
+      { nombre: "STRONGER WITH YOU INTENSELY EDP 100 ML", precio: "$291,500" },
+      { nombre: "STRONGER WITH YOU INTENSELY EDP 150 ML", precio: "$380,200" },
+      { nombre: "STRONGER WITH YOU EDT 100 ML", precio: "$271,200" },
+    ]
+  },
+  ysl: {
+    nombre: "Yves Saint Laurent",
+    productos: [
+      { nombre: "Y MEN EDP 100 ML", precio: null },
+      { nombre: "Y MEN EDT 100 ML", precio: "$250,900" },
+      { nombre: "MYSLF 100 ML", precio: "$321,900" },
+      { nombre: "LA NUIT DE L'HOMME 100 ML", precio: "$250,900" },
+      { nombre: "LIBRE EDP 100 ML", precio: "$304,200" },
+      { nombre: "BLACK OPIUM EDP OVER RED 90 ML", precio: "$354,900" },
+      { nombre: "PARISIENNE L'EAU EDT 90 ML", precio: null },
+    ]
+  },
+  dior: {
+    nombre: "Dior",
+    productos: [
+      { nombre: "DIOR HOMME PARFUM 75 ML", precio: "$380,200" },
+      { nombre: "HOMME INTENSE 100 ML", precio: "$316,800" },
+      { nombre: "SAUVAGE EDP 100 ML", precio: "$309,200" },
+      { nombre: "SAUVAGE EDT 100 ML", precio: "$283,900" },
+      { nombre: "SAUVAGE ELIXIR PARFUM 100 ML", precio: "$514,600" },
+      { nombre: "MISS DIOR EDP 100 ML", precio: "$339,600" },
+    ]
+  },
+  givenchy: {
+    nombre: "Givenchy",
+    productos: [
+      { nombre: "GENTLEMAN EDP 100 ML", precio: "$258,500" },
+      { nombre: "ANGE OU DEMON LE SECRET EDP 100 ML", precio: "$248,400" },
+    ]
+  },
+  dg: {
+    nombre: "Dolce & Gabbana",
+    productos: [
+      { nombre: "FOR MEN EDT 100 ML", precio: "$195,100" },
+      { nombre: "FOR MEN EDT 200 ML", precio: "$215,400" },
+      { nombre: "LIGHT BLUE EDP 100 ML", precio: "$174,900" },
+      { nombre: "THE ONLY ONE EDP INTENSE 100 ML", precio: "$258,500" },
+    ]
+  },
+  versace: {
+    nombre: "Versace",
+    productos: [
+      { nombre: "CRYSTAL NOIR EDT 90 ML", precio: "$202,800" },
+      { nombre: "BRIGHT CRYSTAL ABSOLU EDP", precio: "$228,100" },
+      { nombre: "EROS EDT", precio: "$187,500" },
+      { nombre: "EROS EDP", precio: "$225,600" },
+      { nombre: "EROS PARFUM", precio: "$240,800" },
+      { nombre: "EROS NAJIM POUR HOMME 100 ML", precio: "$240,800" },
+      { nombre: "EROS FLAME EDP 100 ML", precio: "$225,600" },
+    ]
+  },
+  valentino: {
+    nombre: "Valentino",
+    productos: [
+      { nombre: "BORN IN ROMA DONNA EXTRADOSE", precio: "$415,700" },
+      { nombre: "UOMO EDT 100 ML", precio: "$253,500" },
+      { nombre: "UOMO BORN IN ROMA CORAL FANTASY EDT 100 ML", precio: "$329,500" },
+      { nombre: "DONNA BORN IN ROMA EDP 100 ML", precio: "$367,500" },
+      { nombre: "UOMO BORN IN ROMA INTENSE EDP 100 ML", precio: "$380,200" },
+    ]
+  },
+  viktorrolf: {
+    nombre: "Viktor & Rolf",
+    productos: [
+      { nombre: "SPICEBOMB EXTREME EDP 90 ML", precio: "$228,100" },
+      { nombre: "SPICEBOMB INFRARED EDT 90 ML", precio: "$261,100" },
+      { nombre: "SPICEBOMB DARK LEATHER EDP", precio: null },
+    ]
+  },
+  azzaro: {
+    nombre: "Azzaro",
+    productos: [
+      { nombre: "CHROME EDT 100 ML", precio: "$147,000" },
+      { nombre: "THE MOST WANTED PARFUM 100 ML", precio: "$223,000" },
+      { nombre: "THE MOST WANTED EAU DE PARFUM INTENSE", precio: "$243,300" },
+      { nombre: "FOREVER WANTED ELIXIR PARFUM 100 ML", precio: "$309,200" },
+    ]
+  },
+  dumont: {
+    nombre: "Dumont Paris",
+    productos: [
+      { nombre: "NITRO RED POUR HOMME EDP 100 ML", precio: "$141,900" },
+      { nombre: "NITRO WHITE EDP 100 ML", precio: "$126,700" },
+    ]
+  },
+};
